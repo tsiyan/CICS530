e57962578e668a64a98a41e253d56e6a5938265b
jasonchenliang <jasonchenliang@gmail.com>
2014-03-16 23:41:56 -0700

    recommendations screen wireframe

--- Carethy/src/com/carethy/fragment/RecommendationsContentFragment.java
+++ Carethy/src/com/carethy/fragment/RecommendationsContentFragment.java
@@ -1,5 +1,23 @@
 package com.carethy.fragment;
 
-public class RecommendationsContentFragment extends AbstractContentFragment {
+import com.carethy.R;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecommendationsContentFragment extends AbstractContentFragment {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View rootView = inflater.inflate(R.layout.fragment_recommendations, container,
+				false);
+		
+		initView();
+		return rootView;
+	}
+	
+	private void initView(){
+		
+	}
 }
