57c7686c5f4545622246492210297a16799a2b17
haijun <haijunq@gmail.com>
2014-04-01 02:27:18 -0700

    popup window decoration

--- Carethy/src/com/carethy/notification/PopupWindowLayout.java
+++ Carethy/src/com/carethy/notification/PopupWindowLayout.java
@@ -2,12 +2,13 @@ package com.carethy.notification;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.PorterDuff;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,28 +25,35 @@ public class PopupWindowLayout extends LinearLayout {
 		super(context);
 		this.wm = wmr;
 		this.setOrientation(1);
-		this.setBackgroundResource(R.color.nav_drawer_background);
-		this.setPadding(10, 10, 10, 10);
+		this.setBackgroundResource(R.color.actionbar_background);
+		
+		LinearLayout llyt = new LinearLayout(context);
+		llyt.setOrientation(0);
+		ImageView vi = new ImageView(context);
+		vi.setImageResource(R.drawable.ic_launcher);
+		vi.setLayoutParams(new LinearLayout.LayoutParams(150, 150));
+		vi.setPadding(25, 25, 25, 25);
+		llyt.addView(vi);
 		
 		tl = new TextView(context);
 		tl.setText("Carethy");
-		tl.setTextSize(36);
-		tl.setTextColor(getResources().getColor(R.color.actionbar_background));
+		tl.setTextSize(18);
+		tl.setTextColor(getResources().getColor(R.color.actionbar_text));
 		tl.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL);
-		tl.setGravity(Gravity.LEFT);
-		this.addView(tl);
+		tl.setLayoutParams(new LinearLayout.LayoutParams(-1, -1, (float)1.0));
+		tl.setPadding(0, 35, 0, 35);
+		llyt.addView(tl);
+		llyt.setGravity(Gravity.LEFT);
 		
-		View sp = new View(context);
-		sp.setBackgroundResource(R.color.actionbar_background);
-		sp.setLayoutParams(new LinearLayout.LayoutParams(-1, 5));
-		this.addView(sp);
+		this.addView(llyt);
 		
 		tv = new TextView(context);
 		tv.setText(str);
 		tv.setTextSize(18);
-		tv.setTextColor(getResources().getColor(R.color.actionbar_background));
+		tv.setTextColor(Color.BLACK);
+		tv.setBackgroundColor(Color.WHITE);
 		tv.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL);
-		tv.setPadding(10, 10, 10, 10);
+		tv.setPadding(30, 20, 30, 20);
 		this.addView(tv);
 
 		LinearLayout lly = new LinearLayout(context);
@@ -60,7 +68,9 @@ public class PopupWindowLayout extends LinearLayout {
 	        }
 		});
 		btn_dismiss.setLayoutParams(new LinearLayout.LayoutParams(-1, -1, (float) 1.0));
-		btn_dismiss.getBackground().setColorFilter(getResources().getColor(R.color.button), PorterDuff.Mode.MULTIPLY);
+		btn_dismiss.setBackgroundResource(R.color.button);
+		btn_dismiss.setScaleX((float)0.995);
+		btn_dismiss.setPadding(0, 0, 5, 0);
 		lly.addView(btn_dismiss);
 		
 		btn_open = new Button(context);
@@ -75,7 +85,8 @@ public class PopupWindowLayout extends LinearLayout {
 	        }
 		});
 		btn_open.setLayoutParams(new LinearLayout.LayoutParams(-1, -1, (float) 1.0));
-		btn_open.getBackground().setColorFilter(getResources().getColor(R.color.button), PorterDuff.Mode.MULTIPLY);
+		btn_open.setScaleX((float)0.995);
+		btn_open.setBackgroundColor(getResources().getColor(R.color.button));
 		
 		lly.addView(btn_open);
 		
--- Carethy/src/com/carethy/service/ServiceDemo.java
+++ Carethy/src/com/carethy/service/ServiceDemo.java
@@ -1,10 +1,16 @@
 package com.carethy.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.carethy.R;
+import com.carethy.activity.MainActivity;
 import com.carethy.notification.PopupWindow;
 
 public class ServiceDemo extends Service {
@@ -29,7 +35,7 @@ public class ServiceDemo extends Service {
 		Log.i(TAG, "Service onStart--->");
 		// super.onStart(intent, startId);
 		pw.showPopup(this.recommendation);
-
+		this.sendNotification(this.recommendation);
 	}
 
 	@Override
@@ -42,6 +48,40 @@ public class ServiceDemo extends Service {
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
+	}
+	
+	public void sendNotification(String recommendation) {
+		String notificationTitle = "Carethy";
+		String notificationMessage = recommendation;
+
+		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		Intent intent = new Intent(getBaseContext(), MainActivity.class);
+
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+				intent, 0);
+		
+//		Intent dismissIntent = new Intent(this, MainActivity.class);
+//		dismissIntent.setAction("com.carethy.ACTION_DISMISS");
+//		PendingIntent piDismiss = PendingIntent.getService(this, 0, dismissIntent, 0);
+//
+//		Intent snoozeIntent = new Intent(this, MainActivity.class);
+//		snoozeIntent.setAction("com.carethy.ACTION_SNOOZE");
+//		PendingIntent piSnooze = PendingIntent.getService(this, 0, snoozeIntent, 0);
+
+		Notification notif = new NotificationCompat.Builder(this)
+				.setContentTitle(notificationTitle)
+				.setContentText(notificationMessage)
+				.setStyle(new NotificationCompat.BigTextStyle().bigText(notificationMessage))
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentIntent(pendingIntent).setTicker(notificationMessage)
+//				.addAction(R.drawable.ic_action_discard, "Dismiss", piDismiss)
+//				.addAction(R.drawable.ic_recommendations, "Snooze", piSnooze)
+				.build();
+
+		notif.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		notificationManager.notify(0, notif);
 	}
 
 }
